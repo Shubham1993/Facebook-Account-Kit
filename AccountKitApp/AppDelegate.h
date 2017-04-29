081e57b7ba8161ded50e0320f9d9475d808dc6ea
Shubham <shubham@shubhams-mac.local>
2017-04-29 20:11:28 +0530

    Initial Commit

---
+++ AccountKitApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AccountKitApp
+//
+//  Created by Shubham on 29/04/17.
+//  Copyright © 2017 perpule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AccountKitApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AccountKitApp
+//
+//  Created by Shubham on 29/04/17.
+//  Copyright © 2017 perpule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
